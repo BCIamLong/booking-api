8b2c413d4a9630aa303f3119f69c422eb75d59ed
longhoang <[hoalanh21112002@gmail.com]>
2024-05-28 15:58:48 +0700

    ci: fix deployment JS runtime errors

--- src/config/modules.d.ts
+++ src/config/modules.d.ts
@@ -1,8 +1,9 @@
-import { IGuest, IUser } from '~/api/interfaces'
+// * this file is not used because somehow the declare not work maybe we need to create the file in the root level like maybe in src
+// import { IGuest, IUser } from '~/api/interfaces'
 
-declare module 'express-serve-static-core' {
-  // * for authentication and authorization purposes
-  interface Request {
-    user: Omit<IUser, 'passwordConfirm'> | Omit<IGuest, 'passwordConfirm'>
-  }
-}
+// declare module 'express-serve-static-core' {
+//   // * for authentication and authorization purposes
+//   interface Request {
+//     user: Omit<IUser, 'passwordConfirm'> | Omit<IGuest, 'passwordConfirm'>
+//   }
+// }
--- src/data/importData.ts
+++ src/data/importData.ts
@@ -4,13 +4,13 @@ import mongoose from 'mongoose'
 import { Booking, Guest } from '~/api/database/models'
 import { dbConfig } from '~/config'
 
-const { DB_LOCAL } = dbConfig
+const { DB_URI } = dbConfig
 
 const guestsData = JSON.parse(fs.readFileSync('src/data/guests.json', 'utf-8'))
 const bookingsData = JSON.parse(fs.readFileSync('src/data/bookings.json', 'utf-8'))
 ;(async () => {
   try {
-    await mongoose.connect(DB_LOCAL!)
+    await mongoose.connect(DB_URI!)
   } catch (err) {
     console.log(err)
   }
