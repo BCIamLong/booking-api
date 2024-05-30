d3b1834ea7e676b0b7787c9ac947475bfccbc94b
longhoang <[hoalanh21112002@gmail.com]>
2024-05-30 15:53:42 +0700

    update: update cors

--- src/app.ts
+++ src/app.ts
@@ -61,7 +61,7 @@ const corsOptions: CorsOptions = {
 //   optionSuccessStatus: 200
 // }
 
-if (process.env.NODE_ENV === 'production') app.enable('trust proxy')
+// if (process.env.NODE_ENV === 'production') app.enable('trust proxy')
 
 // * default level is -1: which is point to 6
 // * https://www.npmjs.com/package/compression?activeTab=readme
