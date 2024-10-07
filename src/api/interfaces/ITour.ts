28a8355e648899e806288123870c5bc82100ebab
longhoang <longkai21112002@gmail.com>
2024-10-07 09:54:54 +0700

    add: tour interface

---
+++ src/api/interfaces/ITour.ts
@@ -0,0 +1,43 @@
+import { Document } from 'mongoose'
+
+interface StartDate {
+  date: Date
+  participants: number
+  soldOut: boolean
+}
+
+interface StartLocation {
+  type: string
+  coordinates: number[]
+  address: string
+  description: string
+}
+
+interface Location extends StartLocation {
+  day: number
+}
+
+export interface ITourInput {
+  name: string
+  slug: string
+  duration: number
+  maxGroupSize: number
+  difficulty: string
+  type: string
+  ratingsAverage: number
+  ratingsQuantity: number
+  summary: string
+  description: string
+  price: number
+  imageCover: string
+  images: string[]
+  createdAt: Date
+  startDates: StartDate[]
+  vip: boolean
+  startLocation: StartLocation
+  locations: Location[]
+}
+
+export default interface ISetting extends ITourInput, Document {
+  _id: string
+}
